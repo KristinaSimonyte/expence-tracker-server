278ce675c0bf32429cfad99bfeb1d514d304d2a8
Kristina <kristinasimonyte@gmail.com>
2022-07-04 09:39:31 +0300

    some final changes

--- src/helpers/responseHelper.js
+++ src/helpers/responseHelper.js
@@ -1,26 +1,31 @@
-function successResponse(res, data, status = 200) {
+"use strict";
+const bcrypt = require('bcryptjs');
+
+const successResponse = (res, data, status = 200) => {
   res.status(status).json({
     success: true,
     data,
   });
-}
-function failResponse(res, err = 'Something went wrong', status = 500) {
+};
+
+const failResponse = (res, err = 'Something went wrong', status = 500) => {
   res.status(status).json({
     success: false,
     error: [err],
   });
-}
+};
 
-function makeResponce(res, repData, additionalMessage) {
+const makeResponce = (res, repData, additionalMessage) => {
   return !repData.isSuccess
     ? failResponse(res, repData.err)
     : successResponse(
         res,
         !!additionalMessage ? additionalMessage : repData.msg
       );
-}
+};
+
 module.exports = {
-  successResponse,
-  failResponse,
-  makeResponce,
+successResponse,
+failResponse,
+makeResponce,
 };
